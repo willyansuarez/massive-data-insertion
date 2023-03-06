1932dabc3867e30ecf7786423fdd1c1bbf0326db
willian <willyanjosesuarez@gmail.com>
2023-03-06 14:40:57 -0400

    Conexion a la base e importacion de librerias faker y mysql-connector

--- src/main_app.py
+++ src/main_app.py
@@ -1,3 +1,29 @@
+from mysql.connector import Error
+import mysql.connector
 
+
+
+
+try:
+	connection = mysql.connector.connect(
+			host = "localhost",
+			port = 3306,
+			user = "root",
+			password = "",
+			database = "python_inserciones_masivas"
+		)
+
+	if connection.is_connected():
+		print("Conexión establecida")
+	else:
+		print("No se pudo establecer la conexión")
+except Error as ex:
+	print(f"Error durante la conexión{ex}")
+else:
+	pass
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("Conexión cerrada")
 
         
